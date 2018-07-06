4e6e926752c7802d7eabe96f233fe966e533f5bb
Beck <r.c.mcgowan@hotmail.co.uk>
2018-07-07 00:46:36 +0100

    Code tidy up

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;// Required when using Event data.
 
-public class Menu : MonoBehaviour, ISelectHandler// required interface when using the OnSelect method.
+public class Menu : MonoBehaviour, ISelectHandler// required interface when using the OnSelect method. (include IPointerEnterHandler in this list if allowing mouse pointer)
 {
 
     public Text descriptionField;
@@ -12,6 +12,9 @@ public class Menu : MonoBehaviour, ISelectHandler// required interface when usin
 	public void StartGame(string name){
         SceneManager.LoadScene(name);
     }
+
+    //if allowing mouse pointer, use the following function instead of/along with OnSelect:
+    /*public void OnPointerEnter(PointerEventData eventData){}*/
 
     public void OnSelect(BaseEventData eventData)
     {
@@ -39,21 +42,4 @@ public class Menu : MonoBehaviour, ISelectHandler// required interface when usin
     }
 
 }
-
-
-/*
- for future use:
-
-
-
-    string text = "Relaxed Mode:$ $For those who just want to enjoy the game. There are no shells to collect. Wipeouts will only require restarting the current level."
-    text = text.Replace('$','\n');
-
-    string text = "Challenging Mode:$ $For those who like to be frustrated. All shells need to be collected to progress to the next level. Wipeouts or failure to collect all the shells will only require restarting from the start of your current level."
-    text = text.Replace('$','\n');
-
-     string text = "Hardcore Mode:$ $For those who like to be frustrated. All shells need to be collected to progress to the next level. Wipeouts or failure to collect all the shells requires restarting from level 1 again, regardless of progress."
-    text = text.Replace('$','\n');
-     
-     */
 
